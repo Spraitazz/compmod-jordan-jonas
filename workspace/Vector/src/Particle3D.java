74f168124f88b41e29be60df50650af6956f6598
Jonas <you@example.com>
2015-11-03 17:14:41 +0000

    added eclipse workspace folder, up to date

---
+++ workspace/Vector/src/Particle3D.java
@@ -0,0 +1,136 @@
+import java.util.Scanner;
+
+
+public class Particle3D {
+	
+	private Vector3D position;
+	private Vector3D velocity;
+	private double mass;
+	private String label;
+	
+	public void setPosition(Vector3D position){
+		this.position = position;
+	}
+	
+	public void setVelocty(Vector3D velocity){
+		this.velocity = velocity;
+	}
+	
+	public void setMass(double mass) {
+		this.mass = mass;
+	}
+	
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	
+	public Vector3D getPosition() {
+		return position;
+	}
+	
+	public Vector3D getVelocity() {
+		return velocity;
+	}
+	
+	public double getMass() {
+		return mass;
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+	
+	public Particle3D(Vector3D position, Vector3D velocity, double mass, String label) {
+		this.position = position;
+		this.velocity = velocity;
+		this.mass = mass;
+		this.label = label;
+	}
+	
+	public Particle3D() {
+		this.position = null;
+		this.velocity = null;
+		this.mass = Double.NaN;
+		this.label = null;
+	}
+	
+	public String toString(){
+		return "<" + label+"> <"+position.getX()+"> <"+position.getY()+"> <"+position.getZ()+">";
+	}
+	
+	//first position then velocity, expect all components to be input
+	public Particle3D(Scanner scanner){
+		//expecting exactly 7 doubles - check if true
+		double[] vecComp = new double[7];
+		for (int i = 0; i < 7; i++) {
+			if (scanner.hasNextDouble()) {
+				vecComp[i] = scanner.nextDouble();
+			} else {
+				//not a double here - throw error
+			}			
+		}
+		
+		position = new Vector3D(vecComp[0],vecComp[1],vecComp[2]);
+		velocity = new Vector3D(vecComp[3],vecComp[4],vecComp[5]);
+		mass = vecComp[6];
+		label = scanner.next();
+	}
+	
+	public double kineticEnergy() {
+		return 0.5 * mass * velocity.magnitudeSquared();
+	}
+	
+	public void jumpVelocity(double dt, Vector3D force) {
+		//copy necessary as Vector3D force is passed by reference
+		Vector3D forceEffect = new Vector3D(force);
+		forceEffect.scalarMultiply(dt/mass);
+		velocity = Vector3D.vectorAdd(velocity, forceEffect);		
+	}
+	
+	public void jumpPosition(double dt) {
+		Vector3D copyVelocity = new Vector3D(velocity);
+		copyVelocity.scalarMultiply(dt);
+		position = Vector3D.vectorAdd(position, copyVelocity);
+	}
+	
+	public void jumpPosition(double dt, Vector3D force) {
+		Vector3D copyVelocity = new Vector3D(velocity);
+		copyVelocity.scalarMultiply(dt);
+		Vector3D forceEffect = new Vector3D(force);
+		forceEffect.scalarMultiply(dt*dt*0.5/mass);
+		position = Vector3D.vectorAdd(position, copyVelocity);
+		position = Vector3D.vectorAdd(position, forceEffect);
+	}
+	
+	public static Vector3D relativeSeparation(Particle3D p1, Particle3D p2) {
+		Vector3D separation = Vector3D.vectorSubtract(p1.getPosition(), p2.getPosition());
+		return separation;
+	}
+	
+	//force on particle 1 due to particle 2
+	public static Vector3D graviForce(Particle3D p1, Particle3D p2) {
+		Vector3D force = Particle3D.relativeSeparation(p2, p1);
+		force.scalarDivide(Math.pow(force.magnitude(), 3));
+		force.scalarMultiply(p1.getMass()*p2.getMass());
+		return force;
+	}
+	
+	
+	//potential at particle p1 due to particle p2
+	public static double graviPotential(Particle3D p1, Particle3D p2) {
+		return -1 * p1.getMass() * p2.getMass() / Particle3D.relativeSeparation(p2, p1).magnitude();
+	}
+	
+
+	public static void main(String[] args) {
+		/*Vector3D position = new Vector3D(0,0,0);
+		Vector3D velocity = new Vector3D(1,1,1);
+		Particle3D p = new Particle3D(position, velocity, 1, "hi");
+		System.out.println(p);
+		p.jumpPosition(0.5);
+		System.out.println(p);	*/	
+	}
+	
+	
+
+}
---
+++ workspace/Vector/src/Particle3DEuler.java
@@ -0,0 +1,53 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class Particle3DEuler {
+
+	public static void main(String[] args) throws IOException {
+		
+		String outFile = args[0];
+        PrintWriter output = new PrintWriter(new FileWriter(outFile));
+		
+		final double G = 1;
+		
+		Vector3D particlePos = new Vector3D(1,0,0);
+		Vector3D particleVel = new Vector3D(0,1,0);		
+		Particle3D particle = new Particle3D(particlePos, particleVel, 0.0001, "orbiting particle");
+		
+		Vector3D massPos = new Vector3D(0,0,0);
+		Vector3D massVel = new Vector3D(0,0,0);
+		Particle3D mass = new Particle3D(massPos, massVel, 1, "fixed mass");
+		
+		//force on particle due to mass
+		Vector3D force = Particle3D.graviForce(particle, mass);
+		
+		//characteristic time == time for one orbit?
+		int numstep = 10;
+		
+		double dt = 0.1;
+		
+		double t = 0;
+		
+		output.printf("%10.5f %s\n", t, particle);
+		
+		for (int i = 0; i < numstep; i++) {
+			
+			particle.jumpPosition(dt);
+			
+			force = Particle3D.graviForce(particle, mass);
+			
+			particle.jumpVelocity(dt, force);
+			
+			t = t + dt;
+			
+			output.printf("%10.5f %s\n", t, particle);
+			
+		}
+		
+		output.close();		
+		
+	}
+
+}
